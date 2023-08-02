7b541150f1071b968d63f25ebb52b3beb8538083
9000HAL <gsanchezscs@gmail.com>
2023-08-01 17:51:34 -0700

    now .validaators error lol

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,11 @@
 from flask import FlaskForm
+from flask import EmailField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
-
-
-
+class LoginForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit_btn = SubmitField('Log In')
 
 
 
