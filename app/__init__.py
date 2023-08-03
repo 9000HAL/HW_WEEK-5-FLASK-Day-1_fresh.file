ad2291b66dd284917a813149c6a4d65964c64ee6
9000HAL <gsanchezscs@gmail.com>
2023-08-03 12:13:53 -0700

    setting up db

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import Config
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
@@ -10,14 +10,15 @@ app.config.from_object(Config)
 
 
 login_manager = LoginManager()
-db = SQLAlchemy()
-
-
-
 login_manager.init_app(app)
-db.init_app(app)
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+#db.init_app(app) replaced by SQLAlchemy(app) above
 
 
 
-from app import routes
+from app import routes, models
 
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from flask import request, render_template, redirect, url_for, flash
 import requests
 from app.forms import LoginForm, SignUpForm
-from app import app
+from app import app, db
+from app.models import User
+
+
 
 
 @app.route('/')
@@ -10,59 +13,13 @@ def home():
     return render_template('home.html')
 
 
-
-
-REGISTERED_USERS = {
-    'dylank@thieves.com': {
-        'name': 'Dylan',
-        'password': 'ilovemydog'
-    }
-}
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if request.method == 'POST' and form.validate_on_submit():
-        email = form.email.data.lower()
-        password = form.password.data
-        #print(email, password)
-        #return '<h1>Logged In</h1>'
-        if email in REGISTERED_USERS and password == REGISTERED_USERS[email]['password']:
-            flash(f'Welcome back, {REGISTERED_USERS[email]["name"]}!', 'success')
-            return redirect(url_for('home'))
-            #return f"Hello, {REGISTERED_USERS[email]['name']}"
-        else:
-            error = 'INVALID EMAIL OR PASSWORD'
-            return render_template('login.html', form=form, error=error)
-            #return '<h1>Logged In</h1>'
-    else:
-        print('not validated')
-        return render_template('login.html', form=form)
-
-
-
-
-
-
-# sign up form #############################################
-@app.route('/signup' , methods=['GET', 'POST'])
-def signup():
-    form = SignUpForm()
-    if request.method == 'POST' and form.validate_on_submit():
-        #first_name = form.first_name.data
-        #last_name = form.last_name.data
-        name = form.first_name.data + ' ' + form.last_name.data
-        email = form.email.data.lower()
-        password = form.password.data
-        REGISTERED_USERS[email] = {
-            'name': name, 
-            'password': password
-        }
-        print(REGISTERED_USERS)
-        return 'Thank you for signing up'
-    else:
-        return render_template('signup.html', form=form)
+# DELETED BELOW NO LONGER NEEDED ---- DUPLICATE PROCESS----------------------
+#REGISTERED_USERS = {
+#    'dylank@thieves.com': {
+#        'name': 'Dylan',
+#        'password': 'ilovemydog'
+#    }
+#}
 
 
 
@@ -98,3 +55,68 @@ def get_pokemon_data(pokemon_name):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+    # AUTHENTICATION moved to bottom of page----------------------
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        email = form.email.data.lower()
+        password = form.password.data
+        #print(email, password)
+        #return '<h1>Logged In</h1>'
+        if email in REGISTERED_USERS and password == REGISTERED_USERS[email]['password']:
+            flash(f'Welcome back, {REGISTERED_USERS[email]["name"]}!', 'success')
+            return redirect(url_for('home'))
+            #return f"Hello, {REGISTERED_USERS[email]['name']}"
+        else:
+            error = 'INVALID EMAIL OR PASSWORD'
+            return render_template('login.html', form=form, error=error)
+            #return '<h1>Logged In</h1>'
+    else:
+        print('not validated')
+        return render_template('login.html', form=form)
+
+
+
+
+
+
+# sign up form #############################################
+@app.route('/signup' , methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if request.method == 'POST' and form.validate_on_submit():
+
+        #this data is coming from the form for signup
+        user_data = {
+            'first_name': form.first_name.data,
+            'last_name': form.last_name.data,
+            'email': form.email.data.lower(),
+            'password': form.password.data
+        }
+
+        #create user instance
+        new_user = User()
+
+        # set user_data to our USER ATTRIBUTES
+        new_user.from_dict(user_data)
+
+        # save to the database
+        db.session.add(new_user)
+
+        return 'Thank you for signingg up!'
+    else:
+        return render_template('signup.html', form=form)
