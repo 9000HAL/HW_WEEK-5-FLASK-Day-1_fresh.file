f97d7a6093e28011ee2b69df746d218bbadfdd4a
9000HAL <gsanchezscs@gmail.com>
2023-08-03 10:43:08 -0700

    save

--- app/models.py
+++ app/models.py
@@ -6,3 +6,21 @@ from werkzeug.security import generate_password_hash
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password_hash = db.Column(db.String)
+    created_on = db.Column(db.DateTime, default=datetime.utcnow())
+
+    # hashes our password when a user signs up
+    def hash_password(self, signup_password):
+        return generate_password_hash(signup_password)
+
+
+
+    # this method will assign our columns with their respective values
+def from_dict(self, user_data):
+    self.first_name = user_data['first_name']
+    self.last_name = user_data['last_name']
+    self.email = user_data['email']
+    self.password = self.hash_password(user_data['password'])
+
