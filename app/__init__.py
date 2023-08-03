c190984454342a521b9bf1fec0ced0efdf3074e7
9000HAL <gsanchezscs@gmail.com>
2023-08-03 09:31:32 -0700

    install SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from config import Config
+from flask_login import LoginManager
+
+login_manager = LoginManager()
 
 
 app = Flask(__name__)
-
 app.config.from_object(Config)
+login_manager.init_app(app)
+
+
+
 from app import routes
 
