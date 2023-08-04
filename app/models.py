dd277ae4217d12e933cda5830f37c8e5a5ab7d88
9000HAL <gsanchezscs@gmail.com>
2023-08-03 18:04:06 -0700

    everything works???? save--- MAYBE

--- app/models.py
+++ app/models.py
@@ -1,6 +1,80 @@
 from app import db, login_manager
 from flask_login import UserMixin
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password_hash = db.Column(db.String) # Save hashed password.
+    created_on = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+############lecture version below#######################
+
+"""   
+from app import db, login_manager
+from flask_login import UserMixin
+from datetime import datetime
 from werkzeug.security import generate_password_hash
 
 class User(UserMixin, db.Model):
@@ -8,24 +82,27 @@ class User(UserMixin, db.Model):
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
-    password = db.Column(db.String) 
+    password_hash = db.Column(db.String)  
     created_on = db.Column(db.DateTime, default=datetime.utcnow())
 
+
     # hashes our password when a user signs up
-    def hash_password(new_user, signup_password):
+    def hash_password(new_user, signup_password):        
         return generate_password_hash(signup_password)
 
 
 
+
+
+##############
     # this method will assign our columns with their respective values ---- WORK ??? --NO DO NOT USE SELF
-    """
 def from_dict(self, user_data): 
     self.first_name = user_data['first_name']
     self.last_name = user_data['last_name']
     self.email = user_data['email']
     self.password = self.hash_password(user_data['password'])
-    """
-
+    --- WORK ??? --NO DO NOT USE SELF
+##############
 
 # DUPLICATE MODEL TO FIX ERROR ------WORK ?????????? ---YES!!!!!!
 def from_dict(new_user, user_data): 
@@ -38,3 +115,7 @@ def from_dict(new_user, user_data):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
+
+
+
+    """
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,153 @@ import requests
 from app.forms import LoginForm, SignUpForm
 from app import app, db
 from app.models import User
+from werkzeug.security import check_password_hash
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+
+
+
+#############################################pokemon_name#############################################
+@app.route('/pokemon_name', methods=['GET', 'POST'])
+def pokemon_name():
+    pokemon_data = None
+    if request.method == 'POST':
+        pokemon_name = request.form.get('pokemon_name').lower() 
+        pokemon_data = get_pokemon_data(pokemon_name)
+    return render_template('pokemon_name.html', title='Pokemon Page', pokemon_data=pokemon_data)
+
+def get_pokemon_data(pokemon_name):
+    base_url = "https://pokeapi.co/api/v2/"
+    url = base_url + f"pokemon/{pokemon_name}/"
+    response = requests.get(url)
+    data = response.json()
+
+    name = data['name']
+    stats = {stat['stat']['name']: stat['base_stat'] for stat in data['stats']}
+    front_shiny_sprite = data['sprites']['front_shiny']
+    ability = data['abilities'][0]['ability']['name']
+    
+    return {'name': name, 'hp': stats['hp'], 'defense': stats['defense'], 'attack': stats['attack'], 'front_shiny_sprite': front_shiny_sprite, 'ability': ability}
+
+
+
+
+###################################
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        email = form.email.data.lower()
+        password = form.password.data
+        queried_user = User.query.filter(User.email == email).first()
+        if queried_user and queried_user.check_password(password):
+            flash(f'Welcome back {queried_user.first_name}!', 'success')
+            return redirect(url_for('home'))
+        else:
+            error = 'INVALID EMAIL OR PASSWORD'
+            return render_template('login.html', form=form, error=error)
+    else:
+        print('not validated')
+        return render_template('login.html', form=form)
+
+
+
+
+@app.route('/signup' , methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        new_user = User()
+        new_user.first_name = form.first_name.data
+        new_user.last_name = form.last_name.data
+        new_user.email = form.email.data.lower()
+        new_user.set_password(form.password.data) # Setting the hashed password.
+        print("Hashed password:", new_user.password_hash)
+
+
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash(f"Thank you for signing up {new_user.first_name}!", 'success')
+        return redirect(url_for('login'))
+    else:
+        return render_template('signup.html', form=form)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+##################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+############lecture version below#######################
+
+
+
+"""         
+
+
+
+
+
+from flask import request, render_template, redirect, url_for, flash
+import requests
+from app.forms import LoginForm, SignUpForm
+from app import app, db
+from app.models import User
 from werkzeug.security import check_password_hash   
 
 
@@ -81,6 +228,7 @@ def login():
         email = form.email.data.lower()
         password = form.password.data
         queried_user = User.query.filter(User.email == email).first()
+        #if queried_user and check_password_hash(queried_user.password_hash, password):
         if queried_user and check_password_hash(queried_user.password, password):
             flash(f'Welcome back {queried_user.first_name}!', 'success')
             return redirect(url_for('home'))
@@ -93,10 +241,9 @@ def login():
 
 
 
-"""
 
 
-v.2 --- still error----------------------
+v.2 --- still error----------------------do not use this one
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -115,7 +262,6 @@ def login():
         print('not validated')
         return render_template('login.html', form=form)
 
-""" 
 
 
 
@@ -146,6 +292,7 @@ def signup():
         new_user.last_name = user_data['last_name']
         new_user.email = user_data['email']
         new_user.password = new_user.hash_password(user_data['password'])
+
         
         
 
@@ -157,3 +304,7 @@ def signup():
         return redirect(url_for('login'))
     else:
         return render_template('signup.html', form=form)
+
+
+
+"""
